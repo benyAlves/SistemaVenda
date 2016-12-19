822f39a47c020c61a557d3e13a4f267b8c359a1f
dalves <dalves@dalves>
2016-12-19 09:46:34 +0200

    melhoramentos ++ y

--- src/bernardo/venda/visao/LoginView.java
+++ src/bernardo/venda/visao/LoginView.java
@@ -31,7 +31,7 @@ import org.jdesktop.swingx.painter.BusyPainter;
  */
 public class LoginView extends javax.swing.JFrame {
 
-     UtilizadorControle utilizadorControle = new UtilizadorControle();
+    public static UtilizadorControle utilizadorControle = new UtilizadorControle();
     private boolean logado;
     private Utilizador utilizador;
     private CaixaControle caixaControle = new CaixaControle();
@@ -65,17 +65,18 @@ public class LoginView extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        txtUtil = new org.jdesktop.swingx.JXTextField();
         txtSenha = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
         btnEntrar = new javax.swing.JButton();
         processamento = new org.jdesktop.swingx.JXBusyLabel();
         jLabel5 = new javax.swing.JLabel();
+        txtUtil = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 102, 102), 3));
 
@@ -83,15 +84,7 @@ public class LoginView extends javax.swing.JFrame {
         jLabel2.setText("Utilizador:");
 
         jLabel3.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        jLabel3.setText("Senha:");
-
-        txtUtil.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        txtUtil.setPrompt("Informe o utilizador");
-        txtUtil.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txtUtilKeyReleased(evt);
-            }
-        });
+        jLabel3.setText("Palavra-Passe:");
 
         txtSenha.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
         txtSenha.addActionListener(new java.awt.event.ActionListener() {
@@ -128,31 +121,33 @@ public class LoginView extends javax.swing.JFrame {
             }
         });
 
+        txtUtil.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        txtUtil.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtUtilKeyReleased(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(processamento, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txtSenha)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(28, 28, 28)
-                        .addComponent(jLabel2)
+                        .addComponent(btnEntrar, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(txtUtil, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(processamento, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(txtSenha)
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(btnEntrar, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(39, Short.MAX_VALUE))
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtUtil))
+                .addContainerGap(14, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -164,8 +159,8 @@ public class LoginView extends javax.swing.JFrame {
                     .addComponent(processamento, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtUtil, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
+                    .addComponent(jLabel2)
+                    .addComponent(txtUtil, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -183,7 +178,7 @@ public class LoginView extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 36)); // NOI18N
         jLabel1.setText("PEPA");
 
-        jLabel4.setText("versão 1.0");
+        jLabel4.setText("versão 1.1");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -223,18 +218,6 @@ public class LoginView extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void txtUtilKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUtilKeyReleased
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            if (isPreenchido()) {
-                Tarefa t = new Tarefa(processamento, btnEntrar);
-                btnEntrar.setEnabled(false);
-                processamento.setBusy(true);
-                t.execute();
-                processamento.setText("");
-            }
-        }
-    }//GEN-LAST:event_txtUtilKeyReleased
 
     private void txtSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSenhaActionPerformed
 
@@ -278,6 +261,18 @@ public class LoginView extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_btnEntrarKeyReleased
+
+    private void txtUtilKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUtilKeyReleased
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (isPreenchido()) {
+                Tarefa t = new Tarefa(processamento, btnEntrar);
+                btnEntrar.setEnabled(false);
+                processamento.setBusy(true);
+                t.execute();
+                processamento.setText("");
+            }
+        }
+    }//GEN-LAST:event_txtUtilKeyReleased
 
     /**
      * @param args the command line arguments
@@ -365,6 +360,7 @@ public class LoginView extends javax.swing.JFrame {
                 utilizador = utilizadorControle.login(new String(txtSenha.getPassword()), txtUtil.getText());
                 if (utilizador != null) {
                     utilizadorControle = null;
+                    utilizadorLogado = utilizador;
                     CaixaPane.caixa = caixaControle.findStateUser(true, utilizadorLogado);
                     logado = true;
                 }
@@ -404,6 +400,6 @@ public class LoginView extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private org.jdesktop.swingx.JXBusyLabel processamento;
     private javax.swing.JPasswordField txtSenha;
-    private org.jdesktop.swingx.JXTextField txtUtil;
+    private javax.swing.JTextField txtUtil;
     // End of variables declaration//GEN-END:variables
 }
